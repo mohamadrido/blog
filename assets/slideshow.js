81c7a74ad2b68d41a2a530efdf3e4b4cbd343309
mohamadrido <54422386+mohamadrido@users.noreply.github.com>
2021-12-09 11:42:17 +0700

    Update slideshow.js

--- assets/slideshow.js
+++ assets/slideshow.js
@@ -17,10 +17,10 @@ function carousel() {
 // Used to toggle the menu on small screens when clicking on the menu button
 function myFunction() {
   var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
+  if (x.className.indexOf("show") == -1) {
+    x.className += "show";
   } else { 
-    x.className = x.className.replace(" w3-show", "");
+    x.className = x.className.replace("show", "");
   }
 }
 
