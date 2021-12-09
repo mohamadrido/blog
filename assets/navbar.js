9fb38248a551bdeaf3b1b842ecced22a9a371301
mohamadrido <54422386+mohamadrido@users.noreply.github.com>
2021-12-09 11:43:30 +0700

    Update navbar.js

--- assets/navbar.js
+++ assets/navbar.js
@@ -11,18 +11,18 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+        navbar.className = "bar" + "w3card" + "animate-top" + "white";
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        navbar.className = navbar.className.replace(" card animate-top white", "");
     }
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
     var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
+    if (x.className.indexOf("show") == -1) {
+        x.className += "show";
     } else {
-        x.className = x.className.replace(" w3-show", "");
+        x.className = x.className.replace("show", "");
     }
 }
