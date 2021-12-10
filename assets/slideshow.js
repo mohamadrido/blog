910287e0b1a0e68a86d8ea4739c1540d6a8bed0e
mohamadrido <54422386+mohamadrido@users.noreply.github.com>
2021-12-10 13:29:10 +0700

    Update slideshow.js

--- assets/slideshow.js
+++ assets/slideshow.js
@@ -16,40 +16,16 @@ function carousel() {
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function myFunction() {
-  var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
+  var x = document.getElementById("navBar");
+  if (x.className.indexOf("show") == -1) {
+    x.className += " show";
   } else { 
-    x.className = x.className.replace(" w3-show", "");
+    x.className = x.className.replace(" show", "");
   }
 }
 
 // When the user clicks anywhere outside of the modal, close it
 var modal = document.getElementById('sliderModal');
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-// When the user clicks anywhere outside of the modal, close it
-var modal = document.getElementById('parallaxModal');
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-// When the user clicks anywhere outside of the modal, close it
-var modal = document.getElementById('portofolioModal');
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-// When the user clicks anywhere outside of the modal, close it
-var modal = document.getElementById('searchModal');
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
